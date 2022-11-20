356e127747428c920bd566195aeac07a4cd9df5e
LadanyiBalazs <ladanyi-@hotmail.com>
2022-11-20 12:08:15 +0100

    Button hover works!!!

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -1,8 +1,7 @@
-$("#action-btn").on("mousemove", function (e) {
-  const x = e.pageX - e.target.offsetLeft;
-  const y = e.pageY - e.target.offsetTop;
+$(".button").on("mousemove", function (e) {  
+  const x = e.pageX - e.target.offsetLeft - e.target.offsetParent.offsetParent.offsetLeft -e.target.offsetParent.offsetLeft;
+  const y = e.pageY - e.target.offsetTop - e.target.offsetParent.offsetParent.offsetTop - e.target.offsetParent.offsetTop;
 
   e.target.style.setProperty("--x", x + "px");
   e.target.style.setProperty("--y", y + "px");
-  console.log("fuck");
 });
